0002a8569057e3b9db5c95ac136a42e1caefb7a8
Randi Febriadi <randifebriadi@gmail.com>
2025-01-26 16:04:26 +0700

    add shopping cart and modal box

--- js/script.js
+++ js/script.js
@@ -7,6 +7,15 @@ hamburgerMenu.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Ketika class active untuk shopping cart
+const shoppingCartButton = document.querySelector("#shopping-cart-button");
+const shoppingCart = document.querySelector(".shopping-cart");
+
+// Ketika hamburger menut di klik
+shoppingCartButton.onclick = () => {
+  shoppingCart.classList.toggle("active");
+};
+
 // Ketika class active untuk search form
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
@@ -27,4 +36,73 @@ document.addEventListener("click", function (e) {
   if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
+  // untuk menghilangkan shopping cart
+  if (
+    !shoppingCartButton.contains(e.target) &&
+    !shoppingCart.contains(e.target)
+  ) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+const namaProduk = document.querySelector(".modal h3");
+const imageProduk = document.querySelector(".modal img");
+const harga = document.querySelector(".modal .item-price");
+const stars = document.querySelectorAll(".star");
+const dataProduk = [
+  {
+    nama: "Permen Jagoan Neon",
+    stars: 5,
+    harga: "IDR 500",
+    image: "jagoan-neon.jpg",
+    hlama: "IDR 1K",
+  },
+  {
+    nama: "Silkys Marshmallow",
+    stars: 4,
+    harga: "IDR 1K",
+    image: "silkys-marshmallow.jpg",
+    hlama: "IDR 2K",
+  },
+  {
+    nama: "Royale 500",
+    stars: 3,
+    harga: "IDR 500",
+    image: "royale-500.jpg",
+    hlama: "IDR 1K",
+  },
+];
+
+itemDetailButtons.forEach((itemDetailButton, i) => {
+  itemDetailButton.onclick = () => {
+    itemDetailModal.style.display = "flex";
+    namaProduk.textContent = dataProduk[i].nama;
+    imageProduk.setAttribute("src", "img/unggulan/" + dataProduk[i].image);
+    harga.innerHTML =
+      dataProduk[i].harga + ` <span>${dataProduk[i].hlama}</span>`;
+    stars.forEach((star, j) => {
+      if (j < dataProduk[i].stars) {
+        star.classList.add("star-full");
+      } else {
+        star.classList.remove("star-full");
+      }
+    });
+  };
+});
+
+// klik tombol close modal
+const closeButtonModal = document.querySelector(".modal .close-icon");
+
+closeButtonModal.onclick = () => {
+  itemDetailModal.style.display = "none";
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
