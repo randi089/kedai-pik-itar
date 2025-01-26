878283d797c2a54a4d3bb2d363b66b1e51b5ba46
Randi Febriadi <randifebriadi@gmail.com>
2025-01-26 12:05:54 +0700

    half

--- js/script.js
+++ js/script.js
@@ -7,9 +7,24 @@ hamburgerMenu.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
+// Ketika class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search-button");
+
+searchButton.onclick = () => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+};
+
+// Klik di luar sidebar untuk element
 document.addEventListener("click", function (e) {
+  // Untuk menghilangkan nav
   if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  // untuk menghilangkan search
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
